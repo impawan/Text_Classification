7d72ba17b85672ca0c20f3a2dce176f577a6ae1f
impawan <pawanprasad@outlook.com>
2018-04-15 20:19:35 +0530

    Time out  fix

--- new_crawler.py
+++ new_crawler.py
@@ -7,7 +7,7 @@ This is a temporary script file.
 
 import requests
 from lxml import etree
-
+import time
 
 parser = etree.HTMLParser()
 report_name = "assig_desc.csv"
@@ -20,7 +20,12 @@ def indirect_load(FileList):
             create_report(link)
 
 def create_report(url):
-    response = requests.get(url)
+    while True:
+        try : 
+            response = requests.get(url) 
+            break
+        except requests.exceptions.RequestException:
+            time.sleep(300)
     print (url)
     tree = etree.fromstring(response.text, parser)
     assignee = ''.join(tree.xpath("//*[@id='bz_show_bug_column_1']/table/tr[13]/td/span/span/text()")).strip()
@@ -32,7 +37,12 @@ def create_report(url):
     report.write (temp)
 def direct_load():    
     url = 'https://bugs.eclipse.org/bugs/buglist.cgi?bug_status=ASSIGNED&limit=0&order=bug_id&query_format=advanced'
-    response = requests.get(url)
+    while True:
+        try : 
+            response = requests.get(url) 
+            break
+        except requests.exceptions.RequestException:
+            time.sleep(300)
     tree = etree.fromstring(response.text, parser)
     for a in tree.xpath("//*[@id]/td[1]/a"):
         href = ''.join(a.xpath("./@href"))
